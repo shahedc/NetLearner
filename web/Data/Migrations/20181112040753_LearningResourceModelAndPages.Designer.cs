edf9b6dc544f3916f1682b153d75fe9124be66be
shahedc <shahedc@gmail.com>
2018-11-11 23:11:42 -0500

    added migration LearningResourceModelAndPages
    added migration LearningResourceModelAndPages

--- web/Data/Migrations/20181112040753_LearningResourceModelAndPages.Designer.cs
+++ web/Data/Migrations/20181112040753_LearningResourceModelAndPages.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NetLearnerWeb.Data;
 
 namespace NetLearnerWeb.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181112040753_LearningResourceModelAndPages")]
+    partial class LearningResourceModelAndPages
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
