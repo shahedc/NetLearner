03029d85998bbff68c0eb282560c9f17d22132f1
shahedc <shahedc@gmail.com>
2019-01-05 17:11:05 -0500

    added back LearningResources Model + Pages
    added back LearningResources Model + Pages

--- web/Data/ApplicationDbContext.cs
+++ web/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using NetLearnerWeb.Models;
 
 namespace web.Data
 {
@@ -12,5 +13,6 @@ namespace web.Data
             : base(options)
         {
         }
+        public DbSet<NetLearnerWeb.Models.LearningResource> LearningResource { get; set; }
     }
 }
